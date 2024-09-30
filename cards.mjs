ff9d3d6c1c865e2aad3352cf6c323135dae9b887
Kaspar Emanuel <kaspar@kitspace.org>
2024-09-30 15:12:27 +0100

    Fix beats rule

--- cards.mjs
+++ cards.mjs
@@ -24,18 +24,22 @@ let db = d.db_with(d.empty_db(), [
 
 const rules = `[
   [(card ?pip_name ?suit_name ?pip ?suit) [?pip ":pip/name" ?pip_name] [?suit ":suit/name" ?suit_name]]
-  [(beats ?pip1 ?suit1 ?pip2 ?suit2)
+  [(beats ?pip_name1 ?suit_name1 ?pip_name2 ?suit_name2)
+    (card ?pip_name1 ?suit_name1 ?pip1 ?suit1)
+    (card ?pip_name2 ?suit_name2 ?pip2 ?suit2)
     [?pip1 ":pip/value" ?pip_value1]
     [?pip2 ":pip/value" ?pip_value2]
     [?suit1 ":suit/value" ?suit_value1]
     [?suit2 ":suit/value" ?suit_value2]
-    (or [(> ?pip_value1 ?pip_value2)]
-    [(> ?suit_value1 ?suit_value2)])
+    (or
+      [(> ?pip_value1 ?pip_value2)]
+      (and [(= ?pip_value1 ?pip_value2)] [(> ?suit_value1 ?suit_value2)])
+    )
   ]
 ]`;
 
 let result = d.q(
-  `[:find ?pip_name ?suit_name :in $ % :where (card "ace" "hearts" ?pip1 ?suit1) (card ?pip_name ?suit_name ?pip2 ?suit2) (beats ?pip1 ?suit1 ?pip2 ?suit2)]`,
+  `[:find ?pip_name ?suit_name :in $ % :where (beats ?pip_name ?suit_name "ace" "hearts")]`,
 
   db,
   rules,
