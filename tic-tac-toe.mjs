c578ca6a21dfe32f0487a25850bddb4ca30654cc
Kaspar Emanuel <kaspar@kitspace.org>
2024-10-10 01:10:47 +0100

    Tic tac toe init but state is doubling up

--- tic-tac-toe.mjs
+++ tic-tac-toe.mjs
@@ -2,15 +2,15 @@ import d from "datascript";
 
 const schema = {
   ident: { ":db/unique": ":db.unique/identity" },
-  facts: {
+  fact: {
     ":db/valueType": ":db.type/ref",
     ":db/cardinality": ":db.cardinality/many",
   },
 };
 
 let db = d.db_with(d.empty_db(schema), [
-  { control: "oplayer" },
-  { control: "xplayer" },
+  { ident: "control", control: "xplayer" },
+  { ident: "control", control: "oplayer" },
 
   { cell: [1, 1, "b"] },
   { cell: [1, 2, "b"] },
@@ -42,36 +42,75 @@ let db = d.db_with(d.empty_db(schema), [
   { cell: [3, 2, "o"] },
   { cell: [3, 3, "o"] },
 
-  { ident: "init", facts: { cell: [1, 1, "b"] } },
-  { ident: "init", facts: { cell: [1, 2, "b"] } },
-  { ident: "init", facts: { cell: [1, 3, "b"] } },
-  { ident: "init", facts: { cell: [2, 1, "b"] } },
-  { ident: "init", facts: { cell: [2, 2, "b"] } },
-  { ident: "init", facts: { cell: [2, 3, "b"] } },
-  { ident: "init", facts: { cell: [3, 1, "b"] } },
-  { ident: "init", facts: { cell: [3, 2, "b"] } },
-  { ident: "init", facts: { cell: [3, 3, "b"] } },
-  { ident: "init", facts: { control: "xplayer" } },
+  { ident: "init", fact: { cell: [1, 1, "b"] } },
+  { ident: "init", fact: { cell: [1, 2, "b"] } },
+  { ident: "init", fact: { cell: [1, 3, "b"] } },
+  { ident: "init", fact: { cell: [2, 1, "b"] } },
+  { ident: "init", fact: { cell: [2, 2, "b"] } },
+  { ident: "init", fact: { cell: [2, 3, "b"] } },
+  { ident: "init", fact: { cell: [3, 1, "b"] } },
+  { ident: "init", fact: { cell: [3, 2, "b"] } },
+  { ident: "init", fact: { cell: [3, 3, "b"] } },
+  { ident: "init", fact: { control: "xplayer" } },
 
   { ident: "current" },
-
-  //{ ident: "current", facts: { cell: [1, 1, "x"] } },
-  //{ ident: "current", facts: { cell: [1, 2, "x"] } },
 ]);
 
 const rules = `[
-  [(next ?x) [?current "ident" "current"] [(missing? $ ?current "facts")] [?init "ident" "init"] [?init "facts" ?x]]
-  [(next ?x) [_ "current" ?current] [?current "control" "xplayer"] [?x "control" "oplayer"]]
+  [(current-fact ?fact) [?current "ident" "current"] [?current "fact" ?fact]]
+  [(next ?x) [?current "ident" "current"] [(missing? $ ?current "fact")] [?init "ident" "init"] [?init "fact" ?x]]
+
+  [(next ?x) (current-fact ?fact) [?fact "control" "xplayer"] [?x "control" "oplayer"]]
+  [(next ?x) (current-fact ?fact) [?fact "control" "oplayer"] [?x "control" "xplayer"]]
 ]`;
 
-const current = d.q(
-  `[:find ?facts :in $ % :where [?current "ident" "current"] [?current "facts" ?facts]]`,
-  db,
-  rules,
-);
+function logControl(db) {
+  const control = d.q(
+    `[:find ?control :in $ % :where (current-fact ?fact) [?fact "control" ?control]]`,
+    db,
+    rules,
+  );
+  console.log({ control });
+}
 
-console.log(JSON.stringify(current));
+function step(db) {
+  const next = d.q(`[:find ?next :in $ % :where (next ?next) ]`, db, rules);
+  console.log({ next });
 
-const next = d.q(`[:find ?control :in $ % :where (next ?control) ]`, db, rules);
+  const facts = d.q(
+    `[:find ?fact :in $ % :where (current-fact ?fact)]`,
+    db,
+    rules,
+  );
+  console.log({ facts });
 
-console.log(JSON.stringify(next));
+  db = d.db_with(
+    db,
+    facts.map(([id]) => ({ ":db/retract": { ident: "current", fact: id } })),
+  );
+
+  return d.db_with(
+    db,
+    next.map(([id]) => ({ ident: "current", fact: id })),
+  );
+}
+
+
+logControl(db);
+console.log('----')
+console.log('1')
+console.log('----')
+
+db = step(db);
+logControl(db);
+console.log('----')
+console.log('2')
+console.log('----')
+
+
+db = step(db);
+logControl(db);
+console.log('----')
+console.log('3')
+console.log('----')
+
