20fdeed17b7ece0ef43f4f07993750250ffab400
Kaspar Emanuel <kaspar@kitspace.org>
2024-10-11 00:41:48 +0100

    Tic tac toe sort of works

--- tic-tac-toe.mjs
+++ tic-tac-toe.mjs
@@ -5,19 +5,20 @@ const schema = {
   control: {
     ":db/cardinality": ":db.cardinality/one",
   },
-  "0,0": { "db/cardinality": ":db.cardinality/one" },
-  "0,1": { "db/cardinality": ":db.cardinality/one" },
-  "0,2": { "db/cardinality": ":db.cardinality/one" },
-  "1,0": { "db/cardinality": ":db.cardinality/one" },
-  "1,2": { "db/cardinality": ":db.cardinality/one" },
-  "2,0": { "db/cardinality": ":db.cardinality/one" },
-  "2,1": { "db/cardinality": ":db.cardinality/one" },
-  "2,2": { "db/cardinality": ":db.cardinality/one" },
+  "0,0": { ":db/cardinality": ":db.cardinality/one" },
+  "0,1": { ":db/cardinality": ":db.cardinality/one" },
+  "0,2": { ":db/cardinality": ":db.cardinality/one" },
+  "1,0": { ":db/cardinality": ":db.cardinality/one" },
+  "1,2": { ":db/cardinality": ":db.cardinality/one" },
+  "2,0": { ":db/cardinality": ":db.cardinality/one" },
+  "2,1": { ":db/cardinality": ":db.cardinality/one" },
+  "2,2": { ":db/cardinality": ":db.cardinality/one" },
 };
 
 let db = d.db_with(d.empty_db(schema), [
   { control: "player-x" },
   { control: "player-o" },
+
   { type: "cell", "0,0": "blank" },
   { type: "cell", "0,1": "blank" },
   { type: "cell", "0,2": "blank" },
@@ -27,6 +28,7 @@ let db = d.db_with(d.empty_db(schema), [
   { type: "cell", "2,0": "blank" },
   { type: "cell", "2,1": "blank" },
   { type: "cell", "2,2": "blank" },
+
   { type: "cell", "0,0": "x" },
   { type: "cell", "0,1": "x" },
   { type: "cell", "0,2": "x" },
@@ -36,6 +38,7 @@ let db = d.db_with(d.empty_db(schema), [
   { type: "cell", "2,0": "x" },
   { type: "cell", "2,1": "x" },
   { type: "cell", "2,2": "x" },
+
   { type: "cell", "0,0": "o" },
   { type: "cell", "0,1": "o" },
   { type: "cell", "0,2": "o" },
@@ -45,6 +48,17 @@ let db = d.db_with(d.empty_db(schema), [
   { type: "cell", "2,0": "o" },
   { type: "cell", "2,1": "o" },
   { type: "cell", "2,2": "o" },
+
+  { type: "coord", m: 0, n: 0, name: "0,0" },
+  { type: "coord", m: 0, n: 1, name: "0,1" },
+  { type: "coord", m: 0, n: 2, name: "0,2" },
+  { type: "coord", m: 1, n: 0, name: "1,0" },
+  { type: "coord", m: 1, n: 1, name: "1,1" },
+  { type: "coord", m: 1, n: 2, name: "1,2" },
+  { type: "coord", m: 2, n: 0, name: "2,0" },
+  { type: "coord", m: 2, n: 1, name: "2,1" },
+  { type: "coord", m: 2, n: 2, name: "2,2" },
+
   {
     ident: "current",
     control: "player-x",
@@ -64,7 +78,68 @@ const rules = `[
   [(next ?x) [?current "ident" "current"] [?current "control" "player-x"] [?x "control" "player-o"]]
   [(next ?x) [?current "ident" "current"] [?current "control" "player-o"] [?x "control" "player-x"]]
   [(legal ?x) [?current "ident" "current"] [?current "control" "player-x"] [?x "type" "cell"] [?x ?key "x"] [?current ?key "blank"]]
-  [(legal ?x) [?current "ident" "current"] [?current "control" "player-o"] [?x "type" "cell"] [?x ?key "x"] [?current ?key "blank"]]
+  [(legal ?x) [?current "ident" "current"] [?current "control" "player-o"] [?x "type" "cell"] [?x ?key "o"] [?current ?key "blank"]]
+  [(row ?m ?x) 
+    [?current "ident" "current"] 
+    [?coord0 "type" "coord"] 
+    [?coord1 "type" "coord"] 
+    [?coord2 "type" "coord"]
+    [?coord0 "n" 0] 
+    [?coord1 "n" 1] 
+    [?coord2 "n" 2] 
+    [?coord0 "m" ?m] 
+    [?coord1 "m" ?m] 
+    [?coord2 "m" ?m] 
+    [?coord0 "name" ?key0] [?current ?key0 ?x]
+    [?coord1 "name" ?key1] [?current ?key1 ?x]
+    [?coord2 "name" ?key2] [?current ?key2 ?x]
+  ]
+  [(column ?n ?x) 
+    [?current "ident" "current"] 
+    [?coord0 "type" "coord"] 
+    [?coord1 "type" "coord"] 
+    [?coord2 "type" "coord"]
+    [?coord0 "m" 0] 
+    [?coord1 "m" 1] 
+    [?coord2 "m" 2] 
+    [?coord0 "n" ?n] 
+    [?coord1 "n" ?n] 
+    [?coord2 "n" ?n] 
+    [?coord0 "name" ?key0] [?current ?key0 ?x]
+    [?coord1 "name" ?key1] [?current ?key1 ?x]
+    [?coord2 "name" ?key2] [?current ?key2 ?x]
+  ]
+  [(diagonal ?x)
+    [?current "ident" "current"]
+    [?coord0 "type" "coord"]
+    [?coord1 "type" "coord"]
+    [?coord2 "type" "coord"]
+    [?coord0 "m" 0] 
+    [?coord1 "m" 1] 
+    [?coord2 "m" 2] 
+    [?coord0 "n" 0] 
+    [?coord1 "n" 1] 
+    [?coord2 "n" 2] 
+    [?coord0 "name" ?key0] [?current ?key0 ?x]
+    [?coord1 "name" ?key1] [?current ?key1 ?x]
+    [?coord2 "name" ?key2] [?current ?key2 ?x]
+  ]
+  [(diagonal ?x)
+    [?current "ident" "current"]
+    [?coord0 "type" "coord"]
+    [?coord1 "type" "coord"]
+    [?coord2 "type" "coord"]
+    [?coord0 "m" 0] 
+    [?coord1 "m" 1] 
+    [?coord2 "m" 2] 
+    [?coord0 "n" 2] 
+    [?coord1 "n" 1] 
+    [?coord2 "n" 0] 
+    [?coord0 "name" ?key0] [?current ?key0 ?x]
+    [?coord1 "name" ?key1] [?current ?key1 ?x]
+    [?coord2 "name" ?key2] [?current ?key2 ?x]
+  ]
+  [(line ?x) (or (row _ ?x) (column _ ?x) (diagonal ?x))]
 ]`;
 
 function logControl(db) {
@@ -77,11 +152,19 @@ function logControl(db) {
 }
 
 function step(db) {
+  const winner = d.q(
+    `[:find ?x :in $ % :where (line ?x) [(!= ?x "blank")]]`,
+    db,
+    rules,
+  )[0]?.[0];
+  if (winner) {
+    return [winner, db];
+  }
   const legal = d.q(`[:find ?legal :in $ % :where (legal ?legal)]`, db, rules);
 
   const moveId = legal[Math.floor(Math.random() * legal.length)][0];
   const move = toObj(d.entity(db, moveId));
-  console.log("move", move);
+  console.log(move)
 
   db = d.db_with(db, [{ ...move, ident: "current" }]);
 
@@ -91,32 +174,21 @@ function step(db) {
     db,
     next.map(([id]) => {
       const entity = toObj(d.entity(db, id));
+      console.log(entity)
       return { ...entity, ident: "current" };
     }),
   );
-
-  return db;
+  
+  return [winner, db];
 }
 
-logControl(db);
+let winner;
+while (!winner) {
+  [winner, db] = step(db);
+  logControl(db);
+}
 
-console.log("----");
-console.log("1");
-console.log("----");
-db = step(db);
-logControl(db);
-
-console.log("----");
-console.log("2");
-console.log("----");
-db = step(db);
-logControl(db);
-
-console.log("----");
-console.log("3");
-console.log("----");
-db = step(db);
-logControl(db);
+console.log({winner});
 
 function toObj(entity) {
   const entries = entity.entries();
